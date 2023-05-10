103326fedec8fa5d1d415ec21fa3cfd7a9f21a45
chenlongsheng <125951761@qq.com>
2023-05-10 10:41:24 +0800

    first

---
+++ java/com/jeeplus/modules/processData/dao/ProcessDataDao.java
@@ -0,0 +1,44 @@
+package com.jeeplus.modules.processData.dao;
+
+import com.jeeplus.common.persistence.CrudDao;
+import com.jeeplus.common.persistence.MapEntity;
+import com.jeeplus.common.persistence.annotation.MyBatisDao;
+import com.jeeplus.modules.processData.entity.Channels;
+import com.jeeplus.modules.processData.entity.Devices;
+import com.jeeplus.modules.processData.entity.ImageDevCh;
+import com.jeeplus.modules.processData.entity.Images;
+import com.jeeplus.modules.settings.entity.TOrg;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Param;
+
+/**
+ * Created by ZZUSER on 2019/1/4.
+ */
+@MyBatisDao
+public interface ProcessDataDao extends CrudDao<TOrg> {
+
+    void truncateDetail();
+
+    void updateOnline(@Param("mac") String mac, @Param("online") String online);
+
+    void insertDevice(Devices device);//
+
+    void inserChannels(Channels channel);
+
+    void inserImage(Images images);// pdf_org_image 表
+
+    void inserImageDevCh(ImageDevCh ImageDevCh);// pdf_image_dev_ch表
+
+    void updatedevicetail(@Param("id") String id, @Param("online") String online);
+
+    void updateRealData(List<MapEntity> list);
+    List<MapEntity> selectTIdLinkIds();
+
+    List<MapEntity> selectTAlarmPolicys();
+
+
+
+}
